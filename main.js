cf98546d60be2c9925c02b29d6365741fc697578
moe <moe.aoki0124@gmail.com>
2022-02-21 12:30:23 +1000

    set up html and css

---
+++ main.js
@@ -0,0 +1,6 @@
+const celsiusInput = document.getElementById("celsius");
+const fahrenheitInput = document.getElementById("fahrenheit");
+const kelvinInput = document.getElementById("kelvin");
+
+const inputs = document.getElementsByClassName("input");
+
